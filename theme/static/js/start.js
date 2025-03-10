bafe1010774d4685a0a64b80373003f34f571ca4
Mark Steadman <mark@origin.fm>
2025-03-10 16:57:40 +0000

    Adds animation settings to timeline elements

--- theme/static/js/start.js
+++ theme/static/js/start.js
@@ -1,6 +1,22 @@
 import {} from 'bootstrap'
 import AOS from 'aos'
 
+document.querySelectorAll('.section.timeline-section ol').forEach(
+    (ol) => {
+        ol.querySelectorAll('li').forEach(
+            (li, i) => {
+                if (i % 2 == 0) {
+                    li.setAttribute('data-aos', 'fade-right')
+                } else {
+                    li.setAttribute('data-aos', 'fade-left')
+                }
+
+                li.setAttribute('data-aos-delay', 300)
+            }
+        )
+    }
+)
+
 AOS.init(
     {
         offset: 80,

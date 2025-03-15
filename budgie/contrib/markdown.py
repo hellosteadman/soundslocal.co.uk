40086093f8bd18ec7910d8a82fd15fe4fae8b8a7
Mark Steadman <mark@origin.fm>
2025-03-15 09:46:30 +0000

    Adds a Markdown template filter

--- budgie/contrib/markdown.py
+++ budgie/contrib/markdown.py
@@ -3,6 +3,19 @@ from budgie.utils import mark_safe
 from markdown import markdown
 
 
+@app.filter('markdown')
+def markdown_filter(value):
+    if value:
+        return mark_safe(
+            markdown(
+                value.strip(),
+                extensions=['smarty']
+            )
+        )
+
+    return ''
+
+
 @app.transformer('article_body', 100)
 def markdown_transformer(value):
     if value:

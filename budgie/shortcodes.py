a74dc155eedfe442c5927b2bfeed42b6cb1ee5ac
Mark Steadman <mark@origin.fm>
2025-03-25 14:37:15 +0000

    Allows for self-closing shortcodes

--- budgie/shortcodes.py
+++ budgie/shortcodes.py
@@ -1,17 +1,21 @@
 import re
 
 
-SHORTCODE_EX = re.compile(
-    r'\[(\w+)\s*(.*?)\](.*?)\[/\1\]',
+CONTENT_SHORTCODE_EX = re.compile(
+    r'\[(\w+)\s*(.*?)\](.*)\[/\1\]',
     re.MULTILINE
 )
 
+SELFCLOSING_SHORTCODE_EX = re.compile(
+    r'\[(\w+)\s*(.*?)\]',
+    re.MULTILINE
+)
 
 ATTR_EX = re.compile(r'([\w-]+)\s*=\s*"(.*?)"|"(.*?)"')
 
 
-def handle(shortcode, value, func):
-    def replace(match):
+def handle(shortcode, value, func, takes_content=True):
+    def handle_content(match):
         tag, raw_attrs, content = match.groups()
         args = []
         kwargs = {}
@@ -31,4 +35,26 @@ def handle(shortcode, value, func):
 
         return func(content, *args, **kwargs)
 
-    return SHORTCODE_EX.sub(replace, value)
+    def handle_selfclosing(match):
+        tag, raw_attrs = match.groups()
+        args = []
+        kwargs = {}
+
+        if tag != shortcode:
+            return '[%(tag)s %(attrs)s]' % {
+                'tag': tag.strip(),
+                'attrs': raw_attrs.strip()
+            }
+
+        for match in ATTR_EX.findall(raw_attrs):
+            if match[2]:
+                args.append(match[2])
+            else:
+                kwargs[match[0]] = match[1]
+
+        return func(*args, **kwargs)
+
+    if takes_content:
+        return CONTENT_SHORTCODE_EX.sub(handle_content, value)
+
+    return SELFCLOSING_SHORTCODE_EX.sub(handle_selfclosing, value)

890d5bd6518f8df6384e5e7f016f877a3fe6c333
Mark Steadman <mark@origin.fm>
2025-03-27 18:26:18 +0000

    Adds a utility function to get the current datetime in the configured timezone

--- budgie/utils.py
+++ budgie/utils.py
@@ -1,5 +1,12 @@
 from markupsafe import Markup
+from datetime import datetime
+from pytz import timezone
+from .config import settings
 
 
 def mark_safe(value):
     return Markup(value)
+
+
+def now():
+    return datetime.now(timezone(settings.TIMEZONE))
